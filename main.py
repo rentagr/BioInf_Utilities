6b38d97cb1c40754f90f7093ebe1fed8e877ff69
Renata I. Tagirovna <renata.tag.isl@gmail.com>
2025-10-12 18:42:13 +0300

    Modify main.py

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
 """
 Main script for BioInf_Utilities bioinformatics tools
-
-This is the program entry point. Contains only main functions.
 """
 
-# Imports from modules
+# Import main functions from modules
 from modules.dna_rna_tools import run_dna_rna_tools
-from modules.fastq_utils import filter_fastq
+from modules.fastq_utils import filter_fastq_file, filter_fastq_dict
+from bio_files_processor import convert_multiline_fasta_to_oneline, parse_blast_output
+
+
+def main():
+    """
+    Main function that calls all required bioinformatics functions.
+    """
+    # Call DNA/RNA tools function
+    run_dna_rna_tools("ATCG", "reverse_complement")
+
+    # Call FASTQ filtering functions
+    filter_fastq_file("input.fastq", "output.fastq")
+
+    example_sequences = {"seq1": ("ATCG", "IIII")}
+    filter_fastq_dict(example_sequences)
+
+    # Call file processing functions
+    convert_multiline_fasta_to_oneline("input.fasta", "output.fasta")
+    parse_blast_output("blast_results.txt", "best_matches.txt")
+
+
+if __name__ == "__main__":
+    main()
